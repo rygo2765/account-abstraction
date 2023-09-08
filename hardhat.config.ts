a28aae576d9563a46f006ec27fc143fdc91eb555
Ryan Goh <ryangohce@gmail.com>
2023-09-08 15:18:45 +0800

    Configure local node with mainnet fork for testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.15",
+  networks:{
+    fork: {
+      url: process.env.MAINNET_FORK_RPC 
+    }
+  }
 };
 
 export default config;
